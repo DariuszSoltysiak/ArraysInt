e7253c17de3f20572e43f430aca2f0253afa5ff1
DariuszSoltysiak <oneweb@wp.pl>
2023-04-16 10:22:05 +0200

    Dodana opcja ponownego zapytania o liczbę jeśli podana się nie zgadza, ale działa dziwnie

--- src/ArraysInt.java
+++ src/ArraysInt.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 
 public class ArraysInt {
     public static void main(String[] args) {
-        boolean find = false;
+
         int[] arr = {15, -2, 304, 4844, -2255, 66, -777};
 
-        System.out.println("Pass number");
-        Scanner scan = new Scanner(System.in);
-        int seek = scan.nextInt();
 
+        for (int i = 0; i <= arr.length; i++) {
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == seek) {
-                find = true;
+            System.out.println("Pass number");
+            Scanner scan = new Scanner(System.in);
+            int seek = scan.nextInt();
+
+            if (seek == arr[i]) {
+                System.out.println("Number has been found");
                 break;
+            } else {
+                System.out.println("Number hasn`t been found, plese try again");
             }
-        }
-        if (find) {
-            System.out.println("Number has been found");
-        } else {
-            System.out.println("Number hasn`t been found, plese try again");
         }
     }
 }
+
+
 
